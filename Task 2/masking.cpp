b289a07bb66b9811de5d7821b131ca80bc9d6c08
Edwin Toppo <43099097+edwin3105@users.noreply.github.com>
2019-06-14 02:19:46 +0530

    Update masking.cpp

--- Task 2/masking.cpp
+++ Task 2/masking.cpp
@@ -6,7 +6,7 @@ int main(int argv, char** argc)
     Mat im = imread("lane.jpg",IMREAD_COLOR);
     Mat gray,out;
     cvtColor(im,gray,COLOR_BGR2GRAY);
-    Sobel(gray,out,CV_8U,0,1,5);
+    Sobel(gray,out,CV_8U,0,1,5);                     //Sobel Function
     namedWindow("Original",WINDOW_NORMAL);
     namedWindow("Output",WINDOW_NORMAL);
     imshow("Original",im);
