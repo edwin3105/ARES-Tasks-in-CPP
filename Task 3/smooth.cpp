b2b238a980206cf79b215ae0444be84800620f23
Edwin Toppo <43099097+edwin3105@users.noreply.github.com>
2019-06-14 02:30:41 +0530

    Update smooth.cpp

--- Task 3/smooth.cpp
+++ Task 3/smooth.cpp
@@ -5,10 +5,10 @@ int main(int argv, char** argc)
 {
     Mat im = imread("pic.jpg",IMREAD_COLOR);
     Mat avg,gau,med,bil;
-    blur(im,avg,Size(5,5));
-    GaussianBlur(im,gau,Size(5,5),0);
-    medianBlur(im,med,5);
-    bilateralFilter(im,bil,10,110,110);
+    blur(im,avg,Size(5,5));                      //Average blur
+    GaussianBlur(im,gau,Size(5,5),0);            //Gaussian blur
+    medianBlur(im,med,5);                        //Median blur
+    bilateralFilter(im,bil,10,110,110);          //Bilateral filter
     imshow("Original",im);
     imshow("Average",avg);
     imshow("Gaussian",gau);
