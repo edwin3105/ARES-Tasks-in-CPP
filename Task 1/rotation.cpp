eb04ec1bb737a24a81180018d36b4cf19478118f
Edwin Toppo <43099097+nin-ed@users.noreply.github.com>
2019-06-14 15:30:41 +0530

    Update rotation.cpp

--- Task 1/rotation.cpp
+++ Task 1/rotation.cpp
@@ -4,7 +4,7 @@ using namespace cv;
 int main(int argv, char** argc)
 {
     Mat im = imread("pic.jpg",IMREAD_COLOR);
-    Mat M = getRotationMatrix2D(Point(im.cols/2,im.rows/2),60,1);
+    Mat M = getRotationMatrix2D(Point(im.cols/2,im.rows/2),60,1);       // Defining a kernel
     Mat out;
     warpAffine(im,out,M,im.size());
     imshow("Original",im);
