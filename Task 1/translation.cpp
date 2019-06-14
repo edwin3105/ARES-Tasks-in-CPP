5e2ddab99e4c5a57e32f8a1d15764cca5e913e1a
Edwin Toppo <43099097+nin-ed@users.noreply.github.com>
2019-06-14 15:30:06 +0530

    Update translation.cpp

--- Task 1/translation.cpp
+++ Task 1/translation.cpp
@@ -4,7 +4,7 @@ using namespace cv;
 int main(int argv, char** argc)
 {
     Mat im = imread("pic.jpg",IMREAD_COLOR);
-    Mat M = (Mat_<double>(2,3)<<1,0,100,0,1,50);
+    Mat M = (Mat_<double>(2,3)<<1,0,100,0,1,50);              // Defining a kernel
     Mat out;
     warpAffine(im,out,M,im.size());
     imshow("Original",im);
