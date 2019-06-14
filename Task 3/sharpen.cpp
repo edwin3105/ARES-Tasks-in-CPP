1b50cab817f6884f5eef9571ffa66682c4a34036
Edwin Toppo <43099097+nin-ed@users.noreply.github.com>
2019-06-14 15:37:07 +0530

    Update sharpen.cpp

--- Task 3/sharpen.cpp
+++ Task 3/sharpen.cpp
@@ -5,7 +5,7 @@ int main(int argv, char** argc)
 {
     Mat im = imread("pic.jpg",IMREAD_COLOR);
     Mat_<float> M(3,3);
-    M<<-1,-1,-1,-1,9,-1,-1,-1,-1;
+    M<<-1,-1,-1,-1,9,-1,-1,-1,-1;               //Defining a kernel
     Mat out;
     filter2D(im,out,im.depth(),M);              //filter2D function
     imshow("Original",im);
