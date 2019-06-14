71c6109fb53f469d71e4d5f7c7a5245a5ba163b5
Edwin Toppo <43099097+nin-ed@users.noreply.github.com>
2019-06-14 15:09:21 +0530

    Update face_detect.cpp

--- Task 6/face_detect.cpp
+++ Task 6/face_detect.cpp
@@ -15,7 +15,7 @@ int main(int argv, char** argc)
     vector<Rect> det;                                                 // Vector to store detected coordinates
     face.detectMultiScale(gray,det,1.1,3);                            // detectMultiScale function with scale factor 1.1
     for(size_t i=0;i<det.size();i++){
-        rectangle(im,det[i].tl(),det[i].br(),Scalar(255,0,0),2);      // Draws rectangle around face of thickness 2 and color blue
+        rectangle(im,det[i].tl(),det[i].br(),Scalar(255,0,0),2);      // Draws rectangle around face, of thickness 2 and color blue
     }
     imshow("Output",im);
     waitKey();
